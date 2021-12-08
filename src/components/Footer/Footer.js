4cec08de1ca52ffb2c63ae2302a23971b19e19a8
Vanessa-Motta <mottavns@gmail.com>
2021-12-07 21:13:28 -0300

    Api pokemon with paging

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        © Vanessa Motta
+      </p>
+    </div>
+  )
+}
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <img src='pokemonicon.png' />
+
+    </div>
+  );
+}
+
+export default Navbar;
